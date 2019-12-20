564c17580c813a3a97d04f160b8e3a40aa2c378d
builder555 <85308587+builder555@users.noreply.github.com>
2019-12-20 06:25:02 -0500

    refactored, added functionality (query, change db)

--- database.py
+++ database.py
@@ -1,43 +1,57 @@
 import os
 import uuid
 import json
+import copy
 class DBA():
     """Database Adaptor class
     Creates a single file "database". Database will contain a JSON object.
-    
+
     Usage:
         dba = DBA(database_file_name)
     """
-    def __init__(self, collection_name):
+    def __init__(self, collection_name, autocreate=False):
         self.collection_name = collection_name
+        if autocreate and not self.__collection_exists():
+            self.collection_create()
     @property
     def __collection(self):
+        if not self.__collection_exists():
+            raise Exception('Collection does not exist')
         with open(self.collection_name, 'r') as f:
             try:
                 collection = json.load(f)
             except:
                 collection = {}
         return collection
+    def __collection_exists(self):
+        return os.path.exists(self.collection_name)
     def __collection_update(self, new_collection):
         with open(self.collection_name, 'w') as f:
             json.dump(new_collection, f)
+    def collection_change(self, new_collection):
+        self.collection_name = new_collection
     def collection_create(self):
+        if self.__collection_exists():
+            raise Exception('Collection already exists')
         with open(self.collection_name, 'w') as f:
             f.write('')
     def collection_delete(self):
-        if os.path.exists(self.collection_name):
-            os.unlink(self.collection_name)
-    def create_document(self):
+        if not self.__collection_exists():
+            raise Exception('Collection does not exist')
+        os.unlink(self.collection_name)
+    def document_create(self, doc_data={}):
         """Creates a document in a collection
         
         Returns:
             string -- document id
         """
+        _data = copy.deepcopy(doc_data)
         collection = self.__collection
-        unique_id = str(uuid.uuid4())
-        collection[unique_id] = {}
-        with open(self.collection_name, 'w') as f:
-            json.dump(collection, f)
+        unique_id = _data.pop('id', str(uuid.uuid4()))
+        if unique_id in collection:
+            raise Exception('Specified id already exists')
+        collection[unique_id] = _data or {}
+        self.__collection_update(collection)
         return unique_id
     def document_find(self, doc_id):
         """Determines whether specified document exists in collection
@@ -62,6 +76,8 @@ class DBA():
         Returns:
             object -- Full document body
         """
+        if not self.document_find(doc_id):
+            raise Exception('Document does not exist')
         return self.__collection[doc_id]
     def document_update(self, doc_id, doc_data):
         """Updates a specified document in collection
@@ -73,3 +89,10 @@ class DBA():
         collection = self.__collection
         collection[doc_id] = doc_data
         self.__collection_update(collection)
+    def query(self, query):
+        data = self.__collection
+        result = []
+        for key, obj in data.items():
+            if query.items() <= obj.items():
+                result.append({**obj, 'id': key})
+        return result
--- test_database.py
+++ test_database.py
@@ -1,14 +1,49 @@
 import pytest
-from .database import DBA
+from mc.ut.database import DBA
 import os.path
+import copy
+
+first_collect_name = 'first-collection'
+other_collect_name = 'other-collection'
+mock_doc_first = ['a', 'b', 'c']
+mock_doc_other = ['e', 'f', 'g']
+
+class DBAMocked(DBA):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.__data = {
+            first_collect_name: {
+                'test': mock_doc_first
+            },
+            other_collect_name: {
+                'test': mock_doc_other
+            },
+            mock_collection: { },
+        }
+    @property
+    def _DBA__collection(self):
+        return self.__data[self.collection_name]
+    def _DBA__collection_exists(self):
+        return self.collection_name in [other_collect_name, first_collect_name, mock_collection]
+    def _DBA__collection_update(self, new_collection):
+        self.__data[self.collection_name] = new_collection
+
 
 @pytest.fixture
 def mock_collection():
     return '/tmp/temp-collection'
 
 @pytest.fixture
+def FakeDB():
+    return DBAMocked
+
+@pytest.fixture
 def dba(mock_collection):
     return DBA(mock_collection)
+
+@pytest.fixture
+def dba_nofile(FakeDB):
+    return DBAMocked(mock_collection)
 
 @pytest.fixture
 def delete_after(mock_collection):
@@ -31,18 +66,102 @@ def test_db_can_delete_collection(dba, delete_after, mock_collection):
     dba.collection_delete()
     assert os.path.exists(mock_collection) == False
 
-def test_db_can_create_document(dba, setup_cleanup_collection, mock_collection):
-    doc_id = dba.create_document()
+def test_db_cannot_create_collection_twice(dba, delete_after):
+    dba.collection_create()
+    with pytest.raises(Exception):
+        dba.collection_create()
+
+def test_db_cannot_delete_twice(dba, delete_after):
+    dba.collection_create()
+    dba.collection_delete()
+    with pytest.raises(Exception):
+        dba.collection_delete()
+
+def test_db_cannot_delete_non_existent_collection(dba, delete_after):
+    with pytest.raises(Exception):
+        dba.collection_delete()
+
+def test_db_can_autocreate_collection(delete_after, mock_collection):
+    dba = DBA(mock_collection, True)
+    assert dba.collection_delete() is None
+
+def test_db_allows_multiple_calls_to_same_collection(delete_after, mock_collection):
+    dba = DBA(mock_collection, True)
+    dba = DBA(mock_collection, True)
+    assert dba.collection_delete() is None
+
+def test_db_cannot_create_document_without_collection(dba, delete_after):
+    with pytest.raises(Exception):
+        dba.document_create()
+
+def test_db_can_create_document(dba_nofile):
+    doc_id = dba_nofile.document_create()
     assert doc_id != ''
-    assert dba.document_find(doc_id)
+    assert dba_nofile.document_find(doc_id)
 
-def test_db_can_read_document(dba, setup_cleanup_collection):
-    doc_id = dba.create_document()
-    doc = dba.document_read(doc_id)
-    assert doc == {}
+def test_db_can_create_document_with_id_and_data(dba_nofile):
+    data = {
+        'data': 'data',
+        'id': 'abc'
+    }
+    doc_id = dba_nofile.document_create(data)
+    assert doc_id == data['id']
+    assert dba_nofile.document_find(doc_id)
+    del data['id']
+    assert dba_nofile.document_read(doc_id) == data
 
-def test_db_can_update_document(dba, setup_cleanup_collection):
-    doc_id = dba.create_document()
+def test_db_cannot_create_document_with_id_that_exists(dba_nofile):
+    data = {
+        'id': 'abc'
+    }
+    doc_id = dba_nofile.document_create(data)
+    with pytest.raises(Exception):
+        doc_id = dba_nofile.document_create(data)
+
+def test_db_cannot_create_non_object_document(dba_nofile):
+    class InvalidDocClass(): 
+        pass
+    def invalid_doc_function(): 
+        pass
+    with pytest.raises(Exception):
+        dba_nofile.document_create('invalid doc - string')
+    with pytest.raises(Exception):
+        dba_nofile.document_create(['invalid doc - array'])
+    with pytest.raises(Exception):
+        dba_nofile.document_create(15)
+    with pytest.raises(Exception):
+        dba_nofile.document_create(None)
+    with pytest.raises(Exception):
+        dba_nofile.document_create(InvalidDocClass())
+    with pytest.raises(Exception):
+        dba_nofile.document_create(invalid_doc_function)
+
+def test_db_creating_document_does_not_change_original(dba_nofile):
+    data = {
+        'id': 'abc',
+        'name': 'cde',
+    }
+    original_data = copy.deepcopy(data)
+    dba_nofile.document_create(data)
+    assert data == original_data
+
+def test_db_can_check_document_existence(dba_nofile):
+    doc_id = dba_nofile.document_create()
+    assert dba_nofile.document_find(doc_id)
+
+def test_db_can_read_document(dba_nofile):
+    expected_data = {'a': 1}
+    doc_id = dba_nofile.document_create(expected_data)
+    actual_data = dba_nofile.document_read(doc_id)
+    assert actual_data == expected_data
+
+def test_db_cannot_read_non_existing_document(dba_nofile):
+    with pytest.raises(Exception):
+        doc = dba_nofile.document_read('non-existent id')
+
+def test_db_can_update_document(dba_nofile):
+    dba = dba_nofile
+    doc_id = dba.document_create()
     doc_expected = {
         'text': 'field',
         'number': 15,
@@ -52,7 +171,42 @@ def test_db_can_update_document(dba, setup_cleanup_collection):
     doc_actual = dba.document_read(doc_id)
     assert doc_expected == doc_actual
 
-def test_db_can_delete_document(dba, setup_cleanup_collection):
-    doc_id = dba.create_document()
-    dba.document_delete(doc_id)
-    assert dba.document_find(doc_id) == False
+def test_db_can_delete_document(dba_nofile):
+    doc_id = dba_nofile.document_create()
+    dba_nofile.document_delete(doc_id)
+    assert dba_nofile.document_find(doc_id) == False
+
+def test_db_can_query_collection(dba_nofile):
+    items = [
+        {
+            'field1': 'yes',
+            'field2': 'non',
+        },
+        {
+            'field1': 'may',
+            'field2': 'non',
+        },
+        {
+            'field1': 'may',
+            'field2': 'not',
+        },
+    ]
+    for item in items:
+        item_id = dba_nofile.document_create(item)
+        item['id'] = item_id
+    non_existent_data = dba_nofile.query({'field1': 'value'})
+    assert non_existent_data == []
+    expected_single_field_result = [items[1], items[2]]
+    expected_field = 'field1'
+    expected_value = items[1][expected_field]
+    actual_data = dba_nofile.query({expected_field: expected_value})
+    assert actual_data == expected_single_field_result
+    expected_multiple_fields_result = [items[1]]
+    actual_data = dba_nofile.query({'field1': items[1]['field1'], 'field2': items[1]['field2']})
+    assert actual_data == expected_multiple_fields_result
+
+def test_db_can_change_collection(FakeDB):
+    db = FakeDB(first_collect_name)
+    assert db.document_read('test') == mock_doc_first
+    db.collection_change(other_collect_name)
+    assert db.document_read('test') == mock_doc_other
